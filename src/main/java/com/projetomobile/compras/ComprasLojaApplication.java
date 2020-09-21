86aee64d2362dbd559bf1ef16e6fc9f11a8f03d5
Haniel Roanes <h.roanes@gmail.com>
2020-09-21 19:46:19 -0300

    Projeto criado

---
+++ src/main/java/com/projetomobile/compras/ComprasLojaApplication.java
@@ -0,0 +1,13 @@
+package com.projetomobile.compras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComprasLojaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComprasLojaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetomobile/compras/ComprasLojaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetomobile.compras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComprasLojaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
