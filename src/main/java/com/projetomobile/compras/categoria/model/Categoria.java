7dfae5ab5fdb9eb84db050368bbd2a463a3f50e0
Haniel Roanes <h.roanes@gmail.com>
2020-09-21 21:59:11 -0300

    Feita conexão com H2

---
+++ src/main/java/com/projetomobile/compras/categoria/model/Categoria.java
@@ -0,0 +1,31 @@
+package com.projetomobile.compras.categoria.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy  = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	
+	public Categoria() {
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+	
+	
+
+}
